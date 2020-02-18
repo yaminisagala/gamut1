46f32b72d8789c991dbb06381b701c879f33ae25
yamini <yamini@gmai.com>
2020-02-18 17:47:22 +0530

    modfiy logi.java file

--- login.java
+++ login.java
@@ -1,15 +1,1 @@
-new file first edit n commit
-
-
-
-second edit and second commit
-
-new content
-
-<<<<<<< HEAD
-hi
-=======
-making channges frm REMOTE
->>>>>>> a1ffe7bd5ab5a53f7f3b7ff40b1a5ed45fe9123e
-
-ola:
+hi sai
